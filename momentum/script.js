fd1c5cbe2f5436b9021fd14857c3eba886db9546
harin966 <harin966@naver.com>
2024-08-13 23:45:13 +0900

    0813. 기본 문법

--- momentum/script.js
+++ momentum/script.js
@@ -1,1 +1,23 @@
-alert("hi");
+
+const a = 5;     // number
+const b = "2";   // text
+
+const myName = "harin";  // camelCase
+
+const c = 20;   // const : default
+let d = 10;     // let can change(const cannot change)
+d = 12;
+
+var e = 0;      // var(old version)
+
+console.log(a+b);
+console.log("HEllo " + myName);
+console.log(d + 2);
+
+//bool
+const amIFat = true;
+console.log(amIFat);
+
+//null
+const friend = null;
+console.log(friend);
