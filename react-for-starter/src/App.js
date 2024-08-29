677978fd1799df33349d3bd312ba9c8707585e56
harin966 <harin966@naver.com>
2024-08-29 23:07:47 +0900

    [Movie App] useEffect
    - 각각의 js에만 적용 가능한 css 설정 가능(Module.css)
    - 함수가 실행될 때 조건을 가지고 함수를 실행 시킬 수 있다. (useEffect)

--- react-for-starter/src/App.js
+++ react-for-starter/src/App.js
@@ -1,10 +1,34 @@
+//import Button from "./Button";
+//import style from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyWord] = useState("");
+  const onClick = () => setValue((prev) => prev +1);
+  const onchange = (event) => {setKeyWord(event.target.value)};
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' or 'counter' changes");
+  }, [keyword,counter]);
+  //console.log("search for", keyword);
   return (
     <div>
-      <h1>welcome back to react</h1>
+      <input value={keyword} onChange={onchange} type="text" placeholder="Search here..."/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
-
+// state 가 바뀌면 컴포넌트 안의 모든 코드들은 다시 render 된다. 
 export default App;
+
